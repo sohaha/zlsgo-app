74fb47eb8ac5209279c6be2c128aaedae1356cec
seekwe <seekwe@gmail.com>
2024-02-29 15:14:05 +0800

    ✨ feat: 添加GET和POST方法示例

--- internal/controller/index.go
+++ internal/controller/index.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/sohaha/zlsgo/zfile"
 	"github.com/sohaha/zlsgo/znet"
+	"github.com/sohaha/zlsgo/ztype"
+	"github.com/sohaha/zlsgo/zvalid"
 )
 
 type Index struct {
@@ -20,6 +22,28 @@ func (h *Index) Init(r *znet.Engine) error {
 	return nil
 }
 
-func (h *Index) GetError(r *znet.Context) error {
-	return errcode.InvalidInput.WrapText("test Error")
+func (h *Index) GET(r *znet.Context) (ztype.Map, error) {
+	return ztype.Map{"hello": "world"}, nil
+}
+
+type Body struct {
+	Name    string `json:"name"`
+	Age     int    `json:"age"`
+	Address string
+}
+
+func (h *Index) POST(r *znet.Context) (ztype.Map, error) {
+	var body Body
+
+	valid := r.ValidRule().Required()
+	m := map[string]zvalid.Engine{
+		"name":    valid.MinUTF8Length(2, "姓名最短两个字").SetAlias("姓名"),
+		"age":     valid.IsNumber("年龄必须是整数").SetAlias("年龄"),
+		"Address": valid.IsChinese().SetAlias("地址"),
+	}
+	if err := r.BindValid(&body, m); err != nil {
+		return nil, errcode.InvalidInput.WrapErr(err)
+	}
+
+	return ztype.Map{"body": body}, nil
 }
